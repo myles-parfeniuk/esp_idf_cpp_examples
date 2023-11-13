d68d2d819c351268cf310022fab4bb1583441277
myles-parfeniuk <myles.parfenyuk@gmail.com>
2023-11-12 18:32:49 -0800

    Updated button driver example

--- ButtonDriver/pause_button_example/main/main.cpp
+++ ButtonDriver/pause_button_example/main/main.cpp
@@ -17,7 +17,7 @@
 *
 * - Default gpio pins for button one and button two are 25 and 26, respectively. 
 * - Button one is as assumed to be active low with external pull-up being used
-* - Button two is assumed to be active high with external pull-down being used
+* - Button two is assumed to be active low with external pull-up being used
 *
 * Modify the button_config_t struct initializations as necessary. 
 **/
@@ -40,9 +40,9 @@ extern "C" void app_main()
 
     Button::button_config_t button_two_conf =
     {
-        .gpio_num = GPIO_NUM_26, //gpio number 25
-        .active_lo = false, //not active low
-        .active_hi = true, //active high
+        .gpio_num = GPIO_NUM_26, //gpio number 26
+        .active_lo = true, //active low
+        .active_hi = false, //not active high
         .pull_en = false, //no internal pullups/pulldowns
         .long_press_evt_time = 300000, //300ms long-press event generation time
         .held_evt_time = 200000, //200ms held event generation time
@@ -67,7 +67,7 @@ extern "C" void app_main()
                 else
                 {
                   //if presses exceed 10, mute this call-back and reset counted presses
-                  button_one.event.pause(button_one_cb_id);
+                  button_one.event.pause_from_cb(button_one_cb_id);
                   ESP_LOGW("ButtonOneCb", "Counted Presses: %d, button one call-back paused", press_count); 
                   press_count = 0; 
                 }
@@ -82,7 +82,7 @@ extern "C" void app_main()
             //if long press is detected, ignore all other event types
             if(event == Button::ButtonEvent::long_press)
             {
-                button_one.event.un_pause(button_one_cb_id);
+                button_one.event.un_pause_from_cb(button_one_cb_id);
                 ESP_LOGI("ButtonTwoCb", "button one call-back un-paused");
             }
         }
